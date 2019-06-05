f0981b7971bb259be2466f154b2dd57a86044373
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-05 16:11:20 +0530

    5 june

--- controller/addProduct.js
+++ controller/addProduct.js
@@ -5,7 +5,7 @@ const { check,validationResult } = require('express-validator/check');
 var jwt=require("jsonwebtoken");
 var Mongo=require("mongodb");
 var moment = require('moment');
-//var path = require('path');
+var path = require('path');
 
 router.post("/",function(req, res){
 	console.log(req.files);
@@ -28,7 +28,13 @@ router.post("/",function(req, res){
 		// var newname = insertData.date+file.name;
 		var newname = file.name;
 		var filepath = path.resolve("images/"+newname);
-        file.mv(filepath);
+        file.mv(filepath,function(err){
+			if(err){
+				console.log(err);
+				return;
+			}
+	
+		})
         insertData.image = newname;
 		
 		product.insert(insertData, 'product', function(err, result){
