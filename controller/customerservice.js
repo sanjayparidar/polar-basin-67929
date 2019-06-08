b58c8235c15cbba6c78f0699d7b7b6a5e3496daa
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-08 11:14:13 +0530

    8 june

--- controller/customerservice.js
+++ controller/customerservice.js
@@ -6,7 +6,7 @@ router.post('/',function(req,res){
     var data={ };
     admin.insert(req.body,'customerservice',function(err,result){
         data.status=200;
-       data.data=result;
+       data.data=result[0].ops;
        res.send(data)
        
     });
