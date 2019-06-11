a9c7a39151ef1e81b4a418bb6cb6e1ec00373a5f
akash verma <akashverma2792@gmail.com>
2019-06-11 15:13:39 +0530

    11 june

--- controller/signupWebService.js
+++ controller/signupWebService.js
@@ -5,6 +5,7 @@ const Nexmo = require('nexmo');
 var jwt=require("jsonwebtoken");
 const { check,validationResult } = require('express-validator/check');
 var moment = require('moment');
+var requestCurl = require('request');
 
 router.post("/",function (req, res){
 	var request = req.body;
@@ -40,6 +41,18 @@ router.post("/",function (req, res){
 							data.status = 300;
 							res.send(data);
 						}
+						
+						var msge = "Your one time password is : "+insertData.otp;
+						var options = {
+							url: 'http://sms.pnpuniverse.com/api/v4/?api_key=Ae08d6b71079725f135ba5375c69d0abd&method=sms&message='+msge+'&to='+insertData.mobile+'&sender=RICWAL',
+							method: 'POST',
+							headers: {
+								'Content-Type': 'x-ww-form-urlencoded'
+							}
+						};
+						
+						
+						requestCurl(options, function(resreq, resres){});
 						
 						data.message = 'success';
 						data.status = 200;
