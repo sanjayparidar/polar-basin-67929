7a8f5c0ef4dbae3059bf3f1facbe6c2af686e63a
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-14 17:21:45 +0530

    14 jun

--- controller/productseraach.js
+++ controller/productseraach.js
@@ -4,13 +4,13 @@ var search=require("../model/common");
 
 router.post("/",function(req,res){
     if(req.body.category!='' && req.body.searchproduct!=''){
-      var query={$and:[{category:req.body.category},{'product_name': {'$regex': req.body.searchproduct}}]}
+      var query={$and:[{category:req.body.category},{'product_name': {'$regex': req.body.searchproduct,'$options': 'i'}}]}
     }else{
        if(req.body.category!=''){
         var query={category:req.body.category}
        }
        if(req.body.searchproduct){
-             var query= {'product_name': {'$regex': req.body.searchproduct}}
+             var query= {'product_name': {'$regex': req.body.searchproduct,'$options': 'i'}}
       }
     }
     console.log(query,"second")
