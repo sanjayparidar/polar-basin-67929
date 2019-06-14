4cef80fd69674f01a019575c5d9c0c3bc3d27efb
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-14 16:53:12 +0530

    14 jun

--- controller/productseraach.js
+++ controller/productseraach.js
@@ -12,6 +12,7 @@ router.post("/",function(req,res){
        if(req.body.searchproduct){
            console.log(req.body.searchproduct,"first")
            var productname=parseInt('/'+req.body.searchproduct+'/')
+           console.log(productname)
            var query={product_name: productname }
            
        }
