a2539f8987c95072c0bb36c3a1a7bb32cf189083
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-14 18:19:23 +0530

    14 jun

--- controller/orderhistory.js
+++ controller/orderhistory.js
@@ -3,9 +3,11 @@ var router=express.Router();
 var order=require('../model/common');
 
 router.post('/',function(req,res){
-    order.insert(req.body,'orderhistory',function(err,result){
-        var obj={"response":"success"}
-        res.send(obj)
-    });
+    req.body.date = ("0"+new Date().getDate()).slice(-2)+'-'+("0"+(new Date().getMonth()+1)).slice(-2)+'-'+("0"+new Date().getFullYear()).slice(-4)+' '+("0"+new Date().getHours()).slice(-2)+':'+("0"+new Date().getMinutes()).slice(-2);
+    // order.insert(req.body,'orderhistory',function(err,result){
+    //     var obj={"response":"success"}
+    //     res.send(obj)
+    // });
+    res.send(req.body.date)
 });
 module.exports=router
