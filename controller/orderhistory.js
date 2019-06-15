bfc397729b7ce92331fbb545c94bf57ee6c96b0e
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-15 12:22:50 +0530

    14 jun

--- controller/orderhistory.js
+++ controller/orderhistory.js
@@ -33,9 +33,24 @@ router.post('/todayorder',function(req,res){
     var date=moment().format('YYYY-MM-DD');
     var query={$and:[{userid:req.body.userid},{'date': {'$regex':date,'$options': 'i'}}]}
     order.findWhere(query,'orderhistory',function(err,result){
-        res.send(result)
+        if(result.length>0){
+            for(var i=0;i<result.length;i++){
+                productPrice=result[i].productPrice.split(','); 
+                var totalprice= 0;                            
+              for (var i = 0; i < productPrice.length; i++) {  
+                totalprice += parseInt(productPrice[i]);         
+               }
+               result[i].totalprice=totalprice;
+               
+            }
+        }
+        var data={ }
+        data.message="success";
+        data.status=200;
+        data.details=result
+        res.send(data)
     });
 });
 
 
-module.exports=router
+module.exports=router;
