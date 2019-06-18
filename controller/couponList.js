ce3493c0c6571d6dfc082c450bf83bf0403d1436
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-18 17:25:59 +0530

    18 june

--- controller/couponList.js
+++ controller/couponList.js
@@ -14,7 +14,8 @@ router.post("/",function (req, res){
 		var options = {
 			"sort": [['date','desc']],
 			"limit": 10,
-			"skip": (page - 1) * 10
+			"skip": (page - 1) * 10,
+			
 		}
 
 		coupon.findAllByLimit(options, 'coupon', function(err, result){
@@ -35,5 +36,37 @@ router.post("/",function (req, res){
 		res.send(data);
 	}
 });
+
+router.post("/get",function (req, res){
+	var request = req.body;
+	var data={ };
+	if(request.page){
+		var page = request.page;
 		
+		var options = {
+			"sort": [['date','desc']],
+			"limit": 10,
+			"skip": (page - 1) * 10,
+			'end_date': { $lte: new Date() }
+		}
+
+		coupon.findAllByLimit(options, 'coupon', function(err, result){
+			if(err){
+				data.message = 'Invalide Request';
+				data.status = 300;
+				res.send(data);
+			}
+			
+			data.data = result;
+			data.status = 200;
+			res.send(data);
+		});
+		
+	}else{
+		data.message = 'Invalide Request';
+		data.status = 300;
+		res.send(data);
+	}
+});
+
 module.exports=router;
