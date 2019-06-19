d9afddd8a86b13d9a3d80b5d32cd16c13c393b66
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-19 19:16:34 +0530

    19 jun

--- controller/todaydeal.js
+++ controller/todaydeal.js
@@ -3,6 +3,8 @@ var router=express.Router();
 var today =require('../model/common');
 var category =require('../model/common');
 var subcategory =require('../model/common');
+var mongo=require('mongodb')
+
 router.post('/',function(req,res){
    today.insert(req.body,'todaydeal',function(err,result){
        var data={'response':'success'}
@@ -55,4 +57,46 @@ router.post('/find',function(req,res){
 
     })
 })
+
+
+
+router.post('/gettodaydealById',function(req,res){
+    today.findWhere({_id:mongo.ObjectId(req.body.id)},'todaydeal',function(err,result){
+    
+        category.find('product_category',function(err,result1){
+      
+     
+    
+            subcategory.find('subcategory',function(err,result2){
+               for(let i=0;i<result.length;i++){
+                   for(let j=0;j<result1.length;j++){
+                      if(result[i].categoryid==result1[j]._id){
+                          result[i].category=result1[j].category_name
+                      }
+                   }
+                   for(let j=0;j<result2.length;j++){
+                       if(result[i].subcategoryid==result2[j]._id){
+                           result[i].subcategory=result2[j].subcategory
+                       }
+                    }
+                }
+                
+               if(result.length>0){
+                   var data={ }
+                   data.data = result;
+                   data.status = 200;
+                   res.send(data);
+               }else{
+                   var data={ }
+                    data.status=300;
+                    data.response=empty;
+                    res.send(data)
+               }
+            });
+       
+           });
+   
+
+    });
+});
 module.exports=router;
