7e3212990376054704376be9d4f1f5e2e8218a35
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-20 18:38:29 +0530

    14 jun

--- controller/firmworks.js
+++ controller/firmworks.js
@@ -3,10 +3,14 @@ var router=express.Router();
 var workers=require('../model/common');
 
 router.post('/',function(req,res){
-    workers.insert(req.body,'workers',function(err,res){
-       var data={'response':"success"}
-       res.send(data)     
-    });
+    if(req.body.type='link'){
+        workers.insert(req.body,'workers',function(err,res){
+            var data={'response':"success"}
+            res.send(data)     
+         });
+
+    }
+    
 });
 
 
