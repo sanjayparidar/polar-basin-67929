c7ae9532e4d857679fd5ce589b4f3d8e9a80a44f
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-21 11:40:51 +0530

    21 jun

--- controller/orderhistory.js
+++ controller/orderhistory.js
@@ -10,10 +10,11 @@ router.post('/',function(req,res){
     
 
     req.body.orderstatus="ordered"
-    var order =req.body.userid+req.body.date;
-    req.body.orderid="RCW_"+sha1(order)
+    var orderid =req.body.userid+req.body.date;
+    req.body.orderid="RCW_"+sha1(orderid)
     console.log("+++++++=",req.body)
     order.insert(req.body,'orderhistory',function(err,result){
+        
         var obj={"response":"success"}
         res.send(obj)
     });
