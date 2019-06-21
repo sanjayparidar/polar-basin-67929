92bee474a703e07c002cc600e26c84eb07a95962
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-21 13:41:18 +0530

    :20 jun

--- controller/couponDelete.js
+++ controller/couponDelete.js
@@ -4,7 +4,13 @@ var coupon=require('../model/common');
 var mongo=require("mongodb");
 
 router.post('/',function(req,res){
+    console.log(req.body)
     coupon.deleteData({_id:mongo.ObjectId(req.body.id)},'coupon',function(err,result){
+        if(err){
+            console.log("errrrrrrrrrrrrrr",err)
+        }else{
+            console.log("resulttttttttttttttttttttt",result)
+        }
         var data={'response':"success"}
         res.send(data)
     });
