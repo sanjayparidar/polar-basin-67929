edd0f8ffd0990561bccdbfdedd6b9f320b3efdb6
sanjayparidar <sanjaypatidar402@gmail.com>
2019-06-22 16:00:53 +0530

    22 jun

--- controller/sortbycategory.js
+++ controller/sortbycategory.js
@@ -3,27 +3,27 @@ var router=express.Router();
 var category=require('../model/common');
 var subcategory=require('../model/common');
 
-router.get('/',function(err,result){
+router.get('/',function(req,res){
     category.find('product_category',function(err,result){
     subcategory.find('subcategory',function(err,result1){
         if(result.length>0){
         for(i=0;i<result.length;i++){
-            result[i].category=[]
+            result[i].subcategory=[]
             for(j=0;j<result1.length;j++){
                 if(result[i]._id==result1[j].categoryid){
-                   result[i].category.push(result1[j])
+                   result[i].subcategory.push(result1[j])
                 }
             }
         }
         var data={ };
         data.status=200;
         data.data=result;
-        result.send(data)
+        res.send(data)
     }else{
            var data={ };
            data.status=300;
            data.response='empty'
-           result.send(data)
+           res.send(data)
     }
     });
 });
